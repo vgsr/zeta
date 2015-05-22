9cac32ec03dca099c0d37669a04d9e5c38ffd587
Laurens Offereins <lmoffereins@gmail.com>
2015-05-22 16:10:37 +0200

    Remove search form from content-none.php template file

--- content-none.php
+++ content-none.php
@@ -21,12 +21,10 @@
 		<?php elseif ( is_search() ) : ?>
 
 			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'zeta' ); ?></p>
-			<?php get_search_form(); ?>
 
 		<?php else : ?>
 
 			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'zeta' ); ?></p>
-			<?php get_search_form(); ?>
 
 		<?php endif; ?>
 	</div><!-- .page-content -->
