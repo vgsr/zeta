b75dab1b072774fb03301028ca914078ceadeed7
Laurens Offereins <lmoffereins@gmail.com>
2015-12-18 12:11:35 +0100

    VGSR Entity: use new template tags and conditionals as per 1.1.0

--- inc/vgsr-entity.php
+++ inc/vgsr-entity.php
@@ -14,61 +14,31 @@ defined( 'ABSPATH' ) || exit;
 if ( ! function_exists( 'vgsr_entity' ) )
 	return;
 
-/** Navigation *************************************************************/
+/** Template ***************************************************************/
 
 /**
  * Override the template stack for VGSR entities
  *
  * @since 1.0.0
  *
- * @uses vgsr_entity()
+ * @uses is_entity()
+ * @uses is_singular()
  *
  * @param string $template Template file to load
  * @return string Template file
  */
 function zeta_vgsr_entity_template_include( $template ) {
 
-	// Get VGSR Entity
-	$entity = vgsr_entity();
-
-	$post_type = get_post_type();
-
-	// Use `single.php` instead of `page.php` for single entity pages
-	if ( in_array( $post_type, $entity->get_entities() ) && is_singular( $post_type ) ) {
-		$template = get_query_template( $post_type, array( 'single.php' ) );
+	// Entity: use `single.php` instead of the default `page.php`
+	if ( is_entity() && is_singular() ) {
+		$template = get_query_template( get_post_type(), array( 'single.php' ) );
 	}
 
 	return $template;
 }
 add_filter( 'template_include', 'zeta_vgsr_entity_template_include' );
 
-/**
- * Filter the adjacent post navigation label
- *
- * @since 1.0.0
- *
- * @uses vgsr_entity()
- *
- * @param string $label Label
- * @return string Label
- */
-function zeta_vgsr_entity_adjacent_post_navigation_label( $label ) {
-
-	// Get VGSR Entity
-	$entity = vgsr_entity();
-
-	// Previous or next?
-	$previous = ( 'zeta_previous_post_navigation_label' === current_filter() );
-
-	// Bestuur: display bestuur season
-	if ( is_bestuur() && is_singular() ) {
-		$label = $entity->bestuur->get_season( get_adjacent_post( false, '', $previous ) );
-	}
-
-	return $label;
-}
-add_filter( 'zeta_previous_post_navigation_label', 'zeta_vgsr_entity_adjacent_post_navigation_label' );
-add_filter( 'zeta_next_post_navigation_label',     'zeta_vgsr_entity_adjacent_post_navigation_label' );
+/** Entry ******************************************************************/
 
 /**
  * Print entry meta for VGSR entities
@@ -86,8 +56,37 @@ function zeta_vgsr_entity_entry_meta() {
 
 		// Print all entity meta
 		foreach ( vgsr_entity_get_meta() as $key => $args ) {
-			printf( '<span class="%s">%s</span>', "{$type}-{$key}", $args['full'] );
+			printf( '<span class="%s">%s</span>', "{$type}-{$key}", sprintf( $args['label'], $args['value'] ) );
 		}
 	}
 }
 add_action( 'zeta_entry_meta', 'zeta_vgsr_entity_entry_meta' );
+
+/** Navigation *************************************************************/
+
+/**
+ * Filter the adjacent post navigation label
+ *
+ * @since 1.0.0
+ *
+ * @uses is_bestuur()
+ * @uses is_singular()
+ * @uses vgsr_entity()
+ *
+ * @param string $label Label
+ * @return string Label
+ */
+function zeta_vgsr_entity_adjacent_post_navigation_label( $label ) {
+
+	// Previous or next?
+	$previous = ( 'zeta_previous_post_navigation_label' === current_filter() );
+
+	// Bestuur: display bestuur season
+	if ( is_bestuur() && is_singular() ) {
+		$label = vgsr_entity()->bestuur->get( 'season', get_adjacent_post( false, '', $previous ) );
+	}
+
+	return $label;
+}
+add_filter( 'zeta_previous_post_navigation_label', 'zeta_vgsr_entity_adjacent_post_navigation_label' );
+add_filter( 'zeta_next_post_navigation_label',     'zeta_vgsr_entity_adjacent_post_navigation_label' );
