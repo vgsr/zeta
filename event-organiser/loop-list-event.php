90df7e50d1631dea950b1c790125ac0eeb4178d1
Laurens Offereins <lmoffereins@gmail.com>
2016-01-25 16:49:48 +0100

    Event Organiser: use the time_format option for event time display

--- event-organiser/loop-list-event.php
+++ event-organiser/loop-list-event.php
@@ -15,7 +15,7 @@
 				/* translators: 1. Event title 2. Event time */
 				printf( eo_is_all_day() ? '%1$s' : __( '%2$s &mdash; %1$s', 'zeta' ),
 					get_the_title(),
-					eo_get_the_start( _x( 'g:i A', 'Event time title prefix', 'zeta' ) )
+					eo_get_the_start( get_option( 'time_format' ) )
 			); ?>
 		</a>
 	</li>
