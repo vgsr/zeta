6753412241f1810e2eae4824ad6fba58e417a2c8
Laurens Offereins <lmoffereins@gmail.com>
2016-04-05 16:41:05 +0200

    BuddyPress: update members templates with inline docs and 80px avatars in item list

--- buddypress/members/members-loop.php
+++ buddypress/members/members-loop.php
@@ -16,7 +16,14 @@
 
 ?>
 
-<?php do_action( 'bp_before_members_loop' ); ?>
+<?php
+
+/**
+ * Fires before the display of members from the members loop.
+ *
+ * @since 1.2.0
+ */
+do_action( 'bp_before_members_loop' ); ?>
 
 <?php if ( bp_has_members( bp_ajax_querystring( 'members' ) ) ) : ?>
 
@@ -36,15 +43,22 @@
 
 	</div>
 
-	<?php do_action( 'bp_before_directory_members_list' ); ?>
+	<?php
+
+	/**
+	 * Fires before the listing of the members list.
+	 *
+	 * @since 1.1.0
+	 */
+	do_action( 'bp_before_directory_members_list' ); ?>
 
 	<ul id="members-list" class="item-list" role="main">
 
 	<?php while ( bp_members() ) : bp_the_member(); ?>
 
-		<li>
+		<li <?php bp_member_class(); ?>>
 			<div class="item-avatar">
-				<a href="<?php bp_member_permalink(); ?>"><?php bp_member_avatar( array( 'type' => 'full' ) ); ?></a>
+				<a href="<?php bp_member_permalink(); ?>"><?php bp_member_avatar( array( 'width' => 80, 'height' => 80 ) ); ?></a>
 			</div>
 
 			<div class="item">
@@ -58,25 +72,35 @@
 					<?php if ( bp_get_member_latest_update() ) : ?>
 						<span class="update"><?php bp_member_latest_update(); ?></span>
 					<?php endif; ?>
-
 				</div>
 
 				<?php
 
-				 /**
-				  * If you want to show specific profile fields here you can,
-				  * but it'll add an extra query for each member in the loop
-				  * (only one regardless of the number of fields you show):
-				  *
-				  * bp_member_profile_data( 'field=the field name' );
-				  */
+				/**
+				 * Fires inside the listing of an individual member listing item.
+				 *
+				 * If you want to show specific profile fields here you can,
+				 * but it'll add an extra query for each member in the loop
+				 * (only one regardless of the number of fields you show):
+				 *
+				 * bp_member_profile_data( 'field=the field name' );
+				 *
+				 * @since 1.1.0
+				 */
 				do_action( 'bp_directory_members_item' );
 
 				?>
 			</div>
 
 			<div class="action"><?php
+
+				/**
+				 * Fires inside the action section of an individual member listing item.
+				 *
+				 * @since 1.1.0
+				 */
 				do_action( 'bp_directory_members_actions' );
+
 			?></div>
 
 			<button class="action-toggle" aria-controls="actions" aria-expanded="false"><?php _e( 'Actions', 'zeta' ); ?></button>
@@ -86,7 +110,14 @@
 
 	</ul>
 
-	<?php do_action( 'bp_after_directory_members_list' ); ?>
+	<?php
+
+	/**
+	 * Fires after the listing of the members list.
+	 *
+	 * @since 1.1.0
+	 */
+	do_action( 'bp_after_directory_members_list' ); ?>
 
 	<?php bp_member_hidden_fields(); ?>
 
@@ -114,4 +145,11 @@
 
 <?php endif; ?>
 
-<?php do_action( 'bp_after_members_loop' ); ?>
+<?php
+
+/**
+ * Fires after the display of members from the members loop.
+ *
+ * @since 1.2.0
+ */
+do_action( 'bp_after_members_loop' ); ?>
--- buddypress/members/single/member-header.php
+++ buddypress/members/single/member-header.php
@@ -9,6 +9,7 @@
  * - Moved 'bp_profile_header_meta' hook to entry meta {@see zeta_bp_entry_meta()}
  * - Moved user @-mention name and last activity to `zeta_bp_entry_meta()`
  * - Removed #item-meta, brought #latest-update and #item-buttons one level up
+ * - Removed whitespace from #item-buttons
  * - Renamed #item-buttons to #item-actions
  * - Added actions toggle button .item-actions-toggle
  *
@@ -30,7 +31,7 @@ do_action( 'bp_before_member_header' ); ?>
 
 <div id="item-header-avatar">
 	<a class="item-avatar" href="<?php bp_is_my_profile() ? bp_members_component_link( 'profile', 'change-avatar' ) : bp_displayed_user_link(); ?>">
-		<?php bp_displayed_user_avatar( 'type=full' ); ?>
+		<?php bp_displayed_user_avatar( array( 'type' => 'full' ) ); ?>
 	</a>
 
 	<?php if ( bp_is_my_profile() ) : ?>
@@ -87,9 +88,7 @@ do_action( 'bp_before_member_header' ); ?>
  *
  * @since 1.2.0
  */
-do_action( 'bp_after_member_header' ); ?>
-
-<?php
+do_action( 'bp_after_member_header' );
 
 /** This action is documented in bp-templates/bp-legacy/buddypress/activity/index.php */
 do_action( 'template_notices' ); ?>
