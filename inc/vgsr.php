2bcf8aa62a1c480391fe07b7ff0463ba2e6664c3
Laurens Offereins <lmoffereins@gmail.com>
2018-11-12 22:29:23 +0100

    VGSR: show jaargroepen data for ex-eden too

--- inc/vgsr.php
+++ inc/vgsr.php
@@ -22,10 +22,15 @@ if ( ! function_exists( 'vgsr' ) || version_compare( vgsr()->version, '0.1.2', '
  * @since 1.0.0
  */
 function zeta_vgsr_bp_profile_header_meta() {
+
+	// Get displayed user
 	$user_id = bp_displayed_user_id();
 
-	// Profile of VGSR user
-	if ( $user_id && is_user_vgsr( $user_id ) && $jaargroep = vgsr_get_jaargroep( $user_id ) ) {
+	// Determine whether to show details
+	$show_details = $user_id && ( is_user_vgsr( $user_id ) || is_user_exlid( $user_id ) );
+
+	// Jaargroep
+	if ( $show_details && $jaargroep = vgsr_get_jaargroep( $user_id ) ) {
 		echo '<span class="jaargroep">' . sprintf( esc_html__( 'Jaargroep %s', 'vgsr' ), $jaargroep ) . '</span>';
 	}
 }
@@ -37,10 +42,15 @@ add_action( 'bp_profile_header_meta', 'zeta_vgsr_bp_profile_header_meta' );
  * @since 1.0.0
  */
 function zeta_vgsr_bp_directory_members_item() {
+
+	// Get directory member
 	$user_id = bp_get_member_user_id();
 
-	// Profile of VGSR user
-	if ( $user_id && is_user_vgsr( $user_id ) && $jaargroep = vgsr_get_jaargroep( $user_id ) ) {
+	// Determine whether to show details
+	$show_details = $user_id && ( is_user_vgsr( $user_id ) || is_user_exlid( $user_id ) );
+
+	// Jaargroep
+	if ( $show_details && $jaargroep = vgsr_get_jaargroep( $user_id ) ) {
 		echo '<span class="jaargroep">' . sprintf( esc_html__( 'Jaargroep %s', 'vgsr' ), $jaargroep ) . '</span>';
 	}
 }
